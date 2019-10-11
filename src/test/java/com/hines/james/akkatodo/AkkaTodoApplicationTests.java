d52df3ff2171d6d59c02d68d4a36234b030ba827
James Hines <jameshines10@gmail.com>
2019-10-10 21:02:02 -0500

    Adds Greeter and Printer actor classes Adds a test

--- src/test/java/com/hines/james/akkatodo/AkkaTodoApplicationTests.java
+++ src/test/java/com/hines/james/akkatodo/AkkaTodoApplicationTests.java
@@ -1,16 +1,42 @@
 package com.hines.james.akkatodo;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.testkit.javadsl.TestKit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.junit.Assert.assertEquals;
+
 public class AkkaTodoApplicationTests {
 
+	static ActorSystem system;
+
+	@BeforeClass
+	public static void setup() {
+		system = ActorSystem.create();
+	}
+
+	@AfterClass
+	public static void teardown() {
+		TestKit.shutdownActorSystem(system);
+		system = null;
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testGreeterActorSendingOfGreeting() {
+		final TestKit testProbe = new TestKit(system);
+		final ActorRef helloGreeter = system.actorOf(Greeter.props("Hello", testProbe.getRef()));
+
+		helloGreeter.tell(new Greeter.WhoToGreet("Akka"), ActorRef.noSender());
+		helloGreeter.tell(new Greeter.Greet(), ActorRef.noSender());
+		Printer.Greeting greeting = testProbe.expectMsgClass(Printer.Greeting.class);
+
+		assertEquals("Hello, Akka", greeting.message);
 	}
 
 }
