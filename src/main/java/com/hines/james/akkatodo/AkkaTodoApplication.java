64021ce44303d6c5811024ad4a40ad8508c86987
James Hines <jameshines10@gmail.com>
2019-10-10 20:39:26 -0500

    Initial commit

---
+++ src/main/java/com/hines/james/akkatodo/AkkaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.hines.james.akkatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkaTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hines/james/akkatodo/AkkaTodoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hines.james.akkatodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkkaTodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
